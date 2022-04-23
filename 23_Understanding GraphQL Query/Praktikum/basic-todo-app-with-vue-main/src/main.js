66e4bfbbbb8dce5af82274bcd4bc0cad1096764e
Bagasdn115 <bagasdn115@gmail.com>
2022-04-23 14:29:04 +0700

    Update Section 24

--- 23_Understanding GraphQL Query/Praktikum/basic-todo-app-with-vue-main/src/main.js
+++ 23_Understanding GraphQL Query/Praktikum/basic-todo-app-with-vue-main/src/main.js
@@ -6,6 +6,7 @@ import { ApolloClient, HttpLink, InMemoryCache, split } from 'apollo-boost'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
 import Vue from 'vue'
+import { createProvider } from './vue-apollo'
 
 createApp(App).use(router).mount('#app')
 
@@ -14,36 +15,37 @@ const httpLink = new HttpLink({
     uri: 'https://helpful-grouper-31.hasura.app/v1/graphql'
   })
 
-  const wsLink = new WebSocketLink({
-      uri: 'ws://https://helpful-grouper-31.hasura.app/v1/graphql',
-      options: {
-          reconnect: true,
-      }
-  });
+const wsLink = new WebSocketLink({
+    uri: 'ws://https://helpful-grouper-31.hasura.app/v1/graphql',
+    options: {
+        reconnect: true,
+    }
+});
 
-  const link = split(
-      ({ query }) => {
-          const definition = getMainDefinition(query);
-          return definition.kind == "OperationDefinition" && definition.operation == 'subscription';
-      },
-      wsLink,
-      httpLink,
-  );
+const link = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return definition.kind == "OperationDefinition" && definition.operation == 'subscription';
+    },
+    wsLink,
+    httpLink,
+);
 
-  const apolloClient = new ApolloClient({
-      link, 
-      cache: new InMemoryCache(),
-      connectToDevTools: true,
-  });
+const apolloClient = new ApolloClient({
+    link, 
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+});
 
-  const apolloProvider = new VueApollo({
-      defaultClient: apolloClient,
-  });
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+});
 
-  Vue.use(VueApollo);
+Vue.use(VueApollo);
 
-  new Vue({
-      router,
-      apolloProvider,
-      render: function(h) { return h(App) }
-  }).$mount('#app')
+new Vue({
+    router,
+    apolloProvider,
+    apolloProvider: createProvider(),
+    render: function(h) { return h(App) }
+}).$mount('#app')
--- 24_Understanding GraphQL Mutation/summary.md
+++ 24_Understanding GraphQL Mutation/summary.md
@@ -1,10 +1,9 @@
-# Section 23_Understanding GraphQL Query
+# Section 24_Understanding GraphQL Mutation
 ## Resume
 
 ### Dalam materi ini mempelajari :
 	1. GraphQL
 	2. 3 Main feature GraphQL
-   
 
 ## GraphQL
 	GraphQL adalah kueri data sumber terbuka dan manipulation language untuk API, dan runtime untuk memenuhi kueri dengan data yang ada.
@@ -13,3 +12,6 @@
 	Query, adalah suatu bahasa komputer yang digunakan untuk melakukan permintaan terhadap basis data dan sistem informasi.
     Mutations, Digunakan untuk insert, update, delete data.
     Subscription, untuk mendapatkan update data secara realtime.
+
+# Task
+    Task 24 File praktikum section 23
